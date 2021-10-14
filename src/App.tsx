4f89c039b17abb8a6508c9794ad585611f87547b
frisktale <frisktale@live.com>
2021-10-14 10:15:10 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { DefaultButton } from '@fluentui/react/lib/Button';
+import { TextField } from '@fluentui/react/lib/TextField';
+import Avatar from './components/Avatar';
+
+function App() {
+  return (
+    <div className="bg-white">
+      <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
+        <div className="text-center">
+          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
+            Welcome to
+          </h2>
+          <p className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
+            reactjs-vite-tailwindcss-boilerplate
+          </p>
+          <p className="text-xl text-gray-400">Start building for free.</p>
+          <p className="mt-5">
+            <Avatar
+              size="large"
+              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
+            />
+          </p>
+          <p>
+            <DefaultButton className="bg-blue-500 hover:bg-red-500">
+              Hover
+            </DefaultButton>
+          </p>
+          <TextField label="输入" className="focus:ring-4 focus:ring-blue-600" />
+
+          <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ">
+            <p className="text-indigo-600 group-hover:text-gray-900 ">New Project</p>
+            <p className="text-indigo-500 group-hover:text-gray-500 ">Create a new project from a variety of starting templates.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import 'tailwindcss/tailwind.css';
+import App from './App';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
