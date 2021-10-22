320f55e11154957f4d259152b779a4c778b47a14
frisktale <frisktale@live.com>
2021-10-22 20:06:07 +0800

    修改prettier和eslint规则

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <p>
             <DefaultButton
               className="border border-gray-400 rounded px-4 py-2 mt-4 hover:(bg-teal-400 border-teal-400)"
-              onClick={() => setCount((count) => count + 1)}
+              onClick={() => setCount(count => count + 1)}
             >
               count is: {count}
             </DefaultButton>
